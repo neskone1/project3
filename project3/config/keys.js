63ff2befdee719023e7ffffc7367ab00792b92ad
reb2039607 <reb.aguayo@gmail.com>
2019-10-06 23:07:11 -0700

    created database, added passport funtionality, now able to add user

---
+++ project3/config/keys.js
@@ -0,0 +1,11 @@
+//add to .gitignore
+
+module.exports = {
+    google: {
+        clientID: "794209733864-0sh0q593bh0sidd62si1f8ai7tkno8rh.apps.googleusercontent.com",
+        clientSecret: "5BXrP8VGjyUG9Q7eZHf-P-TE"
+    },
+    mongodb: {
+        dbURI: "mongodb://reb2039607:project3@ds349587.mlab.com:49587/heroku_7153thl0"
+    }
+};
--- project3/routes/auth-routes.js
+++ project3/routes/auth-routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const passport = require("passport");
 
 //auth login
 router.get("/login", (req, res) => {
@@ -12,9 +13,14 @@ router.get("/logout", (req, res) => {
 });
 
 //auth with google
-router.get("/google", (req, res) => {
-    //handle with passport
-    res.send("Logging in with Google");
+router.get("/google", passport.authenticate("google", {
+    scope: ["profile"]
+}));
+
+//callback route for google redirect
+router.get("/google/redirect", passport.authenticate("google"), (req, res) => {
+    res.send("You have reached the callback URI");
 });
 
 module.exports = router;
+
