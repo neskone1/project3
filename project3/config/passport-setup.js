64224a6e48a4f2d32f8125b56aff6015a72cbd82
reb2039607 <reb.aguayo@gmail.com>
2019-10-07 21:42:20 -0700

    added serialize/deserialize user function

--- project3/config/passport-setup.js
+++ project3/config/passport-setup.js
@@ -3,6 +3,18 @@ const GoogleStrategy = require("passport-google-oauth20");
 const keys = require("./keys");
 const User = require("../models/user-model");
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    });
+});
+
+
+
 passport.use(
     new GoogleStrategy({
         //options for google strategy
@@ -10,15 +22,24 @@ passport.use(
         clientID: keys.google.clientID, 
         clientSecret: keys.google.clientSecret
     }, (accessToken, refreshToken, profile, done) => {
-        //passport callback function
-        console.log("passport callback function fired");
-        console.log(profile);
-        new User({
-            username: profile.displayName,
-            googleId: profile.id
-        }).save().then((newUser) => {
-            console.log("new user created: " + newUser);
+        //check if user already exists in db
+        User.findOne({googleId: profile.id}).then((currentUser) => {
+            if (currentUser) {
+                //user exists
+                console.log("user is: ", currentUser);
+                done(null, currentUser);
+            } else {
+                //if not, create user in db
+                new User({
+                    username: profile.displayName,
+                    googleId: profile.id
+                }).save().then((newUser) => {
+                    console.log("new user created: " + newUser);
+                    done(null, newUser);
+                });
+            }
         });
+        
     })
 )
 
