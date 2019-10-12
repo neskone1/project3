0b4069b88de82410c0522ae825bac73e9b9debb1
James Smit <jamessmit1222@gmail.com>
2019-10-12 13:03:03 -0700

    started working on changing news feed into looks with slideshow

--- project3/src/pages/newsFeed.js
+++ project3/src/pages/newsFeed.js
@@ -4,6 +4,17 @@ import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
 import "./style.css";
+import ReactDOM from "react-dom";
+import { Gallery, GalleryImage } from "react-gesture-gallery";
+
+const images = [
+  "https://images.unsplash.com/photo-1559666126-84f389727b9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1356&q=80",
+  "https://images.unsplash.com/photo-1557389352-e721da78ad9f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+  "https://images.unsplash.com/photo-1553969420-fb915228af51?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1049&q=80",
+  "https://images.unsplash.com/photo-1550596334-7bb40a71b6bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+  "https://images.unsplash.com/photo-1550640964-4775934de4af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+];
+
 
 
 
@@ -18,7 +29,7 @@ function newsFeed () {
           </Col>
         </Row>
         <Row>
-          <Col size="md-12">
+          <Col size="lg-12">
     <div id="news-feed" > 
     <div className= "img-container">
     <img alt={ ""}
