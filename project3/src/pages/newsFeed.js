a2624f0ebda6e903c2d56deb52390b4936fe8f3e
James Smit <jamessmit1222@gmail.com>
2019-10-15 20:00:52 -0700

    adding in slideshow

--- project3/src/pages/newsFeed.js
+++ project3/src/pages/newsFeed.js
@@ -1,60 +1,81 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import { Gallery, GalleryImage } from "react";
 
 import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
 import "./style.css";
-import ReactDOM from "react-dom";
-import { Gallery, GalleryImage } from "react-gesture-gallery";
 
 const images = [
-  "https://images.unsplash.com/photo-1559666126-84f389727b9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1356&q=80",
-  "https://images.unsplash.com/photo-1557389352-e721da78ad9f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-  "https://images.unsplash.com/photo-1553969420-fb915228af51?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1049&q=80",
-  "https://images.unsplash.com/photo-1550596334-7bb40a71b6bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-  "https://images.unsplash.com/photo-1550640964-4775934de4af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+  "https://image.goat.com/crop/3500/attachments/micropost_pictures/images/017/071/235/original/temp1545076054.jpeg",
+  "https://image.goat.com/crop/3500/attachments/micropost_pictures/images/016/697/676/original/temp1543954087.jpeg",
+  "https://image.goat.com/crop/3500/attachments/micropost_pictures/images/026/582/293/original/temp1569809675.jpeg",
+  "https://image.goat.com/crop/3500/attachments/micropost_pictures/images/017/078/236/original/temp1545087503.jpeg",
+  "https://image.goat.com/crop/3500/attachments/micropost_pictures/images/013/769/006/original/temp1533578886.jpeg",
+  "https://image.goat.com/crop/3500/attachments/micropost_pictures/images/021/685/991/original/temp1558404697.jpeg"
 ];
 
 
-
-
-
-function newsFeed () {
-    return(
-        <div>
-        <Container style={{ marginTop: 20, }}>
+function newsFeed() {
+  return (
+    <div>
+      <Container style={{ marginTop: 20, }}>
         <Row>
           <Col size="md-12">
             <h1>Looks</h1>
           </Col>
         </Row>
         <Row>
-          <Col size="lg-12">
-    <div id="news-feed" > 
-    <div className= "img-container">
-    <img alt={ ""}
-    src={"https://image.goat.com/crop/750/attachments/micropost_pictures/images/026/629/485/original/temp1569904146.jpeg"}/>
-    </div>
-    
-    
-    <div className="content">
-   
-    
-    </div>
 
-    </div>
-    
-    
+          <div className="content">
 
-    </Col>
+          </div>
+
         </Row>
-    </Container>
-      
-     
-      </div>
-      );
+      </Container>
+
+    </div>
+  );
+}
+
+
+
+function App() {
+  const [index, setIndex] = React.useState(0);
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      if (index === 4) {
+        setIndex(0);
+      } else {
+        setIndex(prev => prev + 1);
       }
-      
-      
+    }, 3000);
+    return () => clearInterval(timer);
+  }, [index]);
+
+  return (
+    <Gallery
+      style={{
+        background: "black",
+        height: "100vh",
+        width: "100vw"
+      }}
+      index={index}
+      onRequestChange={i => {
+        setIndex(i);
+      }}
+    >
+      {images.map(image => (
+        <GalleryImage objectFit="contain" key={image} src={image} />
+      ))}
+    </Gallery>
+  );
+}
+
+const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
+
+
 
 export default newsFeed;
