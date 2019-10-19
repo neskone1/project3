0675dace6fba8437b34c025ad985ce424bb8a2de
James Smit <jamessmit1222@gmail.com>
2019-10-19 11:59:04 -0700

    adding images to slide show working on fixing image size

--- project3/src/components/Slideshow/slideshow.js
+++ project3/src/components/Slideshow/slideshow.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 // import ReactDOM from "react-dom";
-import { Gallery, GalleryImage } from "react";
-
-const images = [
+// import { Gallery, GalleryImage } from "react";
+import { Fade } from 'react-slideshow-image';
+const fadeImages = [
     "https://image.goat.com/crop/3500/attachments/micropost_pictures/images/017/071/235/original/temp1545076054.jpeg",
     "https://image.goat.com/crop/3500/attachments/micropost_pictures/images/016/697/676/original/temp1543954087.jpeg",
     "https://image.goat.com/crop/3500/attachments/micropost_pictures/images/026/582/293/original/temp1569809675.jpeg",
@@ -10,43 +10,102 @@ const images = [
     "https://image.goat.com/crop/3500/attachments/micropost_pictures/images/013/769/006/original/temp1533578886.jpeg",
     "https://image.goat.com/crop/3500/attachments/micropost_pictures/images/021/685/991/original/temp1558404697.jpeg"
   ];
-
-class Slideshow extends Component {
-    const [index, setIndex] = React.useState(0);
-    React.useEffect(() => {
-      const timer = setInterval(() => {
-        if (index === 4) {
-          setIndex(0);
-        } else {
-          setIndex(prev => prev + 1);
-        }
-      }, 3000);
-      return () => clearInterval(timer);
-    }, [index]);
-    
-    render() {
-        return (
-        <Gallery
-            style={{
-            background: "black",
-            height: "50vh",
-            width: "50vw"
-            }}
-            index={index}
-            onRequestChange={i => {
-            setIndex(i);
-            }}
-        >
-            {images.map(image => (
-            <GalleryImage objectFit="contain" key={image} src={image} />
-            ))}
-        </Gallery>
-        
-        );
-            }
+  const fadeProperties = {
+    duration: 5000,
+    transitionDuration: 500,
+    infinite: false,
+    indicators: true,
+    onChange: (oldIndex, newIndex) => {
+      console.log(`fade transition from ${oldIndex} to ${newIndex}`);
+    }
   }
+  
+  const Slideshow = () => {
+    return (
+      <div className="slide-container">
+        <Fade {...fadeProperties}>
+          <div className="each-fade">
+            <div className="image-container">
+              <img src={fadeImages[0]} />
+            </div>
+            
+          </div>
+          <div className="each-fade">
+            <div className="image-container">
+              <img src={fadeImages[1]} />
+            </div>
+            
+          </div>
+          <div className="each-fade">
+            <div className="image-container">
+              <img src={fadeImages[2]} />
+            </div>
+            
+          </div>
+          <div className="each-fade">
+            <div className="image-container">
+              <img src={fadeImages[3]} />
+            </div>
+            
+          </div>
+          <div className="each-fade">
+            <div className="image-container">
+              <img src={fadeImages[4]} />
+            </div>
+            
+          </div>
+          <div className="each-fade">
+            <div className="image-container">
+              <img src={fadeImages[5]} />
+            </div>
+            
+          </div>
+          <div className="each-fade">
+            <div className="image-container">
+              <img src={fadeImages[6]} />
+            </div>
+            
+          </div>
+        </Fade>
+      </div>
+    )
+  }
+// class Slideshow extends Component {
+//     const [index, setIndex] = React.useState(0);
+//     React.useEffect(() => {
+//       const timer = setInterval(() => {
+//         if (index === 4) {
+//           setIndex(0);
+//         } else {
+//           setIndex(prev => prev + 1);
+//         }
+//       }, 3000);
+//       return () => clearInterval(timer);
+//     }, [index]);
+    
+//     render() {
+//         return (
+//         <Gallery
+//             style={{
+//             background: "black",
+//             height: "50vh",
+//             width: "50vw"
+//             }}
+//             index={index}
+//             onRequestChange={i => {
+//             setIndex(i);
+//             }}
+//         >
+//             {images.map(image => (
+//             <GalleryImage objectFit="contain" key={image} src={image} />
+//             ))}
+//         </Gallery>
+        
+//         );
+//             }
+//   }
 
-const rootElement = document.getElementById("root");
-// ReactDOM.render(<Slideshow />, rootElement);
+// const rootElement = document.getElementById("root");
+// // ReactDOM.render(<Slideshow />, rootElement);
 
   export default Slideshow;
